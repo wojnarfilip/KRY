5bd7d922e5a836001cd5b98b9d3fd9f7f6d8564e
wojnarfilip <fwojnar@redhat.com>
2023-04-19 16:45:46 +0200

    Logger

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# KRY
+# KRY - Kryptografie
 
+# Requirements
+
+1. You have to have python 3.7+ installed
+2. You need python libraries specified in Requirements file
+   1. pip install eciespy
+   2. pip install tinyec
+   3. pip install pycryptodome
+   4. pip install ecdsa
 
 # How to run
 
-Simply run clientBob then clientAlice
+Simply run clientBob.py then clientAlice.py
--- src/EllipticCurves/ECDSA.py
+++ src/EllipticCurves/ECDSA.py
@@ -1,22 +1,21 @@
 from ecdsa import SigningKey, VerifyingKey, SECP256k1
-import hashlib
+
 
 def generate_ECDSA_keys(sk_pem_name, vk_pem_name):
     sk = SigningKey.generate(curve=SECP256k1)
     vk = sk.verifying_key
     vk.precompute()
 
-    with open(sk_pem_name, "wb") as f:
+    with open("CryptoKeys/" + sk_pem_name, "wb") as f:
         f.write(sk.to_pem())
-    with open(vk_pem_name, "wb") as f:
+    with open("CryptoKeys/" + vk_pem_name, "wb") as f:
         f.write(vk.to_pem())
+
 
 def sign_message(message, private_pem):
     sk = SigningKey.from_pem(private_pem)
     return sk.sign(message)
-    # with open(private_pem) as f:
-    #     sk = SigningKey.from_pem(f.read(), hashlib.sha256)
-    #     return sk.sign(message)
+
 
 def verify_message(message, signature, public_pem):
     with open(public_pem) as f:
